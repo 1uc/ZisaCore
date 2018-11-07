909a49a888afab52122381310277fc013a66be1e
Luc Grosheintz <luc.grosheintz@gmail.com>
2018-11-07 08:57:15 +0100

    Implement `indent_block` to indent every line.

---
+++ include/zisa/utils/indent_block.hpp
@@ -0,0 +1,8 @@
+#ifndef INDENT_BLOCK_H_RL3BG
+#define INDENT_BLOCK_H_RL3BG
+
+namespace zisa {
+std::string indent_block(int by, const std::string &text);
+} // namespace zisa
+
+#endif /* end of include guard */
---
+++ src/zisa/utils/indent_block.cpp
@@ -0,0 +1,14 @@
+#include <cassert>
+#include <string>
+
+#include <zisa/utils/indent_block.hpp>
+#include <zisa/utils/replace_all.hpp>
+
+namespace zisa {
+
+std::string indent_block(int by, const std::string &text) {
+  std::string white_space(static_cast<std::size_t>(2 * by), ' ');
+  return white_space + replace_all(text, "\n", "\n" + white_space);
+}
+
+} // namespace zisa
