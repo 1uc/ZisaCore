514b81f350bb24f7d82a89d0db2a8e08beb55e86
Luc Grosheintz <luc.grosheintz@gmail.com>
2018-12-05 19:27:26 +0100

    Add `has_key` for dictionary/map like objects.

---
+++ include/zisa/utils/has_key.hpp
@@ -0,0 +1,14 @@
+#ifndef HAS_KEY_H_9TX4O
+#define HAS_KEY_H_9TX4O
+
+namespace zisa {
+
+template <class Map, class Key>
+auto has_key(const Map &map, const Key &key)
+    -> decltype(map.find(key) != map.end()) {
+  return map.find(key) != map.end();
+}
+
+} // namespace zisa
+
+#endif
