0db03c7bb6fd636a30e732adcdc7f1e2cdb81ed7
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-05-01 17:34:06 +0200

    Pretty print bytes.

---
+++ include/zisa/utils/human_readable_size.hpp
@@ -0,0 +1,19 @@
+#ifndef ZISA_HUMAN_READABLE_SIZE_BVOQUE
+#define ZISA_HUMAN_READABLE_SIZE_BVOQUE
+
+#include <string>
+#include <zisa/utils/string_format.hpp>
+
+namespace zisa {
+
+/// Returns the size as a human readable string.
+/** Examples:
+ *      human_readable_size(345) == "345 B"
+ *      human_readable_size(3456) == "3.45 KB"
+ *      human_readable_size(3456600) == "3.45 MB"
+ */
+std::string human_readable_size(size_t size_in_bytes,
+                                const std::string &float_format = "%.2f");
+
+}
+#endif
---
+++ src/zisa/utils/human_readable_size.cpp
@@ -0,0 +1,30 @@
+#include <zisa/utils/human_readable_size.hpp>
+
+#include <array>
+#include <cmath>
+
+#include <iostream>
+
+namespace zisa {
+std::string human_readable_size(size_t size_in_bytes,
+                                const std::string &number_format) {
+  if (size_in_bytes == 0) {
+    return "0 B";
+  }
+
+  char prefixes[] = {'K', 'M', 'G', 'T', 'P'};
+  double logb = log10(double(size_in_bytes));
+
+  auto atol = double(2ul << 20);
+  auto k = int(round((logb / 3.0) * atol) / atol);
+
+  if (k == 0) {
+    return string_format("%d B", size_in_bytes);
+  } else {
+    double b = round(double(size_in_bytes) / pow(10.0, 3.0 * k) * atol) / atol;
+    return string_format(number_format, b)
+           + string_format(" %cB", prefixes[k - 1]);
+  }
+}
+
+}
