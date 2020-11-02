d40e6ebcd312e377684423e25567b5f430f028ab
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-11-02 08:16:36 +0100

    Add wrapper to `std::chrono::high_resolution_clock`.

---
+++ include/zisa/utils/timer.hpp
@@ -0,0 +1,35 @@
+//
+// Created by lucg on 10/30/20.
+//
+
+#ifndef ZISA_TIMER_HPP
+#define ZISA_TIMER_HPP
+
+#include <chrono>
+#include <zisa/config.hpp>
+
+namespace zisa {
+
+class Timer {
+private:
+  using clock = std::chrono::high_resolution_clock;
+
+protected:
+  auto now() const { return clock::now(); }
+
+public:
+  Timer() : t_start(now()) {}
+
+  void reset() { t_start = now(); }
+
+  inline double elapsed_seconds() const {
+    return std::chrono::duration<double>(now() - t_start).count();
+  }
+
+private:
+  clock::time_point t_start;
+};
+
+}
+
+#endif // ZISA_TIMER_HPP
