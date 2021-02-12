398ede233395eb76c9ec8da4b234483e4f6cdda7
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-12 16:31:48 +0100

    Update dependency script.

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+set -e
 
 if [[ "$#" -ne 2 ]]
 then
@@ -6,18 +7,24 @@ then
 fi
 
 component_name="ZisaCore"
-zisa_core_root=$(realpath $(dirname $(readlink -f $0))/..)
+zisa_core_root="$(realpath "$(dirname "$(readlink -f "$0")")"/..)"
 
-install_dir=$(${zisa_core_root}/bin/install_dir.sh $1 $2)
-source_dir=${install_root}/sources/
+install_dir="$("${zisa_core_root}/bin/install_dir.sh" "$1" "$2")"
 
-conan_file=${zisa_core_root}/conanfile.txt
+conan_file="${zisa_core_root}/conanfile.txt"
 
-if [[ -f $conan_file ]]
+if [[ -f "$conan_file" ]]
 then
-   mkdir -p ${install_dir}/conan && cd ${install_dir}/conan
-   conan install $conan_file -s compiler.libcxx=libstdc++11
+   mkdir -p "${install_dir}/conan" && cd "${install_dir}/conan"
+   conan install "$conan_file" -s compiler.libcxx=libstdc++11
 fi
 
 echo "The dependencies were installed at"
 echo "    ${install_dir}"
+echo ""
+echo "Use"
+echo "    cmake -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake \\ "
+echo "          -DCMAKE_MODULE_PATH=${install_dir}/conan \\ "
+echo "          -DCMAKE_C_COMPILER=${CC} \\ "
+echo "          -DCMAKE_CXX_COMPILER=${CXX} \\ "
+echo "          REMAINING_ARGS "
--- bin/install_dir.sh
+++ bin/install_dir.sh
@@ -1,17 +1,21 @@
 #! /usr/bin/env bash
 
+set -e
+
 if [[ "$#" -ne 2 ]]
 then
     echo "Usage: $0 COMPILER DESTINATION"
+    exit -1
 fi
 
-component_name="ZisaCore"
+compiler="$1"
+compiler_id="$(basename "${compiler}")"
+compiler_version="$("$compiler" -dumpversion)"
 
-compiler=$1
-compiler_id=$(basename ${compiler})
-compiler_version=$($compiler -dumpversion)
+build_mangled_name="${compiler}__${compiler_version}"
+build_sha="$(echo "${build_mangled_name}" | sha1sum | head -c 6)"
 
-install_root=$2
-install_dir=${install_root}/${component_name}/${compiler_id}/${compiler_version}
+install_root="${PWD}/$2"
+install_dir="${install_root}/${build_sha}"
 
-echo ${install_dir}
+echo "${install_dir}"
