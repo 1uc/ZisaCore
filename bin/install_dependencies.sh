6d9fb1a4fa822b75b1f7ea946a15b15112726ab5
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-07-18 15:08:41 +0200

    Add `--print_install_dir`

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -3,14 +3,31 @@ set -e
 
 if [[ "$#" -ne 2 ]]
 then
-    echo "Usage: $0 COMPILER DESTINATION"
+    echo "Usage: $0 COMPILER DESTINATION [--print_install_dir]"
     exit -1
 fi
+
+for arg in "$@"
+do
+    case $arg in
+        --print_install_dir)
+            PRINT_INSTALL_PATH=1
+            ;;
+        *)
+            ;;
+    esac
+done
 
 component_name="ZisaCore"
 zisa_core_root="$(realpath "$(dirname "$(readlink -f "$0")")"/..)"
 
 install_dir="$("${zisa_core_root}/bin/install_dir.sh" "$1" "$2")"
+
+if [[ ${PRINT_INSTALL_PATH} -eq 1 ]]
+then
+  echo $install_dir
+  exit 0
+fi
 
 conan_file="${zisa_core_root}/conanfile.txt"
 
