47bef1fcedfca7b700b95e2573cd60c094faa295
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-01-01 13:09:24 +0100

    Update `catch2` to v3.

--- include/zisa/testing/testing_framework.hpp
+++ include/zisa/testing/testing_framework.hpp
@@ -2,4 +2,5 @@
 // Copyright (c) 2021 ETH Zurich, Luc Grosheintz-Laval
 
 // The precise include has changed in the past. Hence this "indirection".
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
